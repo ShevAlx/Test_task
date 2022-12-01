0c57067d10c8429d8b2493ed4b27c61d257ec840
ShevAlx <119549311+ShevAlx@users.noreply.github.com>
2022-11-30 18:52:04 -0800

    finish main block

--- test-task/src/main.js
+++ test-task/src/main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import banner from './img/banner.jpg';
 import tick from './img/tick.svg';
+import clock from './img/clock.svg';
+import thumbs_up from './img/thumbs-up.svg';
+
 
 const Main = () => {
     return (
@@ -27,6 +30,41 @@ const Main = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='advantages'>
+                        <div className='advantages__block'>
+                            <img className='advantages__icon' src={clock}/>
+                            <h4 className='advantages__title'>
+                                Money as soon as next business day
+                            </h4>
+                            <p className='advantages__text'>
+                                With funds in your account as soon as the next business day*, you can get moving on your big plans
+                            </p>
+                        </div>
+                        <div className='advantages__block'>
+                            <img className='advantages__icon' src={thumbs_up}/>
+                            <h4 className='advantages__title'>
+                                We consider all types of credit history 
+                            </h4>
+                            <p className='advantages__text'>
+                                Just because your credit score may be "not-so-great" doesn't mean you can't get approved.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='request'>
+                    <h3 className='request__title'>
+                        Checkout faster
+                    </h3>
+                    <p className='request__text'>
+                        Our pre-filled form lets you request a loan faster, so you can get on with your day.
+                    </p>
+                    <input className='request__input' placeholder="Email Address"></input>
+                        <span className='request__trigger'>
+                            Start here
+                        </span>
+                    <button className='request__btn'>
+                        Get Started
+                    </button>
                 </div>
             </div>
         </div>
